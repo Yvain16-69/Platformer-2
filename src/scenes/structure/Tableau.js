2ab82629eac5ebf9a4cb6b3cc2721605c2ad7c11
Yvain Di Costanzo <72392571+Yvain16-69@users.noreply.github.com>
2021-04-01 12:27:05 +0200

    Réparation

--- src/scenes/structure/Tableau.js
+++ src/scenes/structure/Tableau.js
@@ -17,16 +17,39 @@ class Tableau extends Phaser.Scene{
      preload(){
         this.load.image('sky', 'assets/Fond.png');
         this.load.image('blood', 'assets/blood.png');
-        this.load.spritesheet('player','assets/GokuS.png');
-	    
-        //this.player=new Player(this,0,0);
+        this.load.spritesheet('player',
+            'assets/GokuS.png',
+            { frameWidth: 32, frameHeight: 48}
+        );
+        this.load.spritesheet('stance',
+            'assets/player_stance.png',
+            { frameWidth: 32, frameHeight: 48 }
+        );
+     }
+     create(){
+         Tableau.current=this;
+         this.sys.scene.scale.lockOrientation("landscape")
+         console.log("On est sur "+this.constructor.name+" / "+this.scene.key);
+         /**
+          * Le ciel en fond
+          * @type {Phaser.GameObjects.Image}
+          */
+        this.sky=this.add.image(0, 0, 'Fond').setOrigin(0,0);
+        this.sky.displayWidth=14*64;
+        this.sky.setScrollFactor(0,0);
+        /**
+         * Le joueur
+         * @type {Player}
+         */
+        this.player=new Player(this,0,0);
 
         this.blood=this.add.sprite(this.sys.canvas.width/2,this.sys.canvas.height/2,"blood")
         this.blood.displayWidth=64;
         this.blood.displayHeight=64;
         this.blood.visible=false;
+        this.blood.setDepth(1000);
 
-    }
+     }
     update(){
         super.update();
         this.player.move();
@@ -64,10 +87,34 @@ class Tableau extends Phaser.Scene{
     ramasserEtoile (player, star)
     {
         star.disableBody(true, true);
-	
+        ui.gagne();
+
+        //va lister tous les objets de la scène pour trouver les étoies et vérifier si elles sont actives
+        let totalActive=0;
+        for(let child of this.children.getChildren()){
+            if(child.texture && child.texture.key==="star"){
+                if(child.active){
+                    totalActive++;
+                }
+            }
+        }
         if(totalActive===0){
             this.win();
         }
+    }
+
+    /**
+     * Aïeee ça fait mal
+     * @param player
+     * @param spike
+     */
+    hitSpike (player, spike)
+    {
+        this.physics.pause();
+        player.setTint(0xff0000);
+        player.anims.play('turn');
+        this.scene.restart();
+
     }
 
 
@@ -116,4 +163,61 @@ class Tableau extends Phaser.Scene{
         }
     
     }
+
+
+/**
+     * Pour reset cette scène proprement
+     * @private
+     */
+ _destroy(){
+    this.player.stop();
+    this.scene.stop();
 }
+
+/**
+ * Quand on a gagné
+ */
+win(){
+    Tableau.suivant();
+}
+
+/**
+ * Va au tableau suivant
+ */
+static suivant(){
+    let ceSeraLaSuivante=false;
+    let nextScene=null;
+    if(Tableau.current){
+        for(let sc of game.scene.scenes){
+            if(sc.scene.key !== "ui"){
+                if(!nextScene){
+                    if(ceSeraLaSuivante){
+                        nextScene=sc;
+                    }
+                    if(sc.scene.key === Tableau.current.scene.key){
+                        ceSeraLaSuivante=true;
+                    }
+                }
+            }
+        }
+    }
+    if(!nextScene){
+        nextScene = game.scene.scenes[0];
+    }
+    Tableau.goTableau(nextScene);
+}
+
+static goTableau(tableau){
+    if(Tableau.current){
+        Tableau.current._destroy();
+    }
+    game.scene.start(tableau);
+}
+
+}
+
+/**
+* Le tableau en cours
+* @type {null|Tableau}
+*/
+Tableau.current=null;
